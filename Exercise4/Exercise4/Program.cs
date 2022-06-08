3c535704030542eb21416fbc4ad02bdf8dbafeb0
Joshua Cole <16261287+bebeseal@users.noreply.github.com>
2022-06-07 22:36:28 -0500

    Committing projects

---
+++ Exercise4/Exercise4/Program.cs
@@ -0,0 +1,18 @@
+﻿using System;
+
+namespace Exercise4
+{
+    class MainClass
+    {
+        public static void Main(string[] args)
+        {
+            Console.Write("Enter first altitude: ");
+            int altitude1 = int.Parse(Console.ReadLine());
+
+            Console.Write("Enter Second Altitude: ");
+            int altitude2 = int.Parse(Console.ReadLine());
+
+            Console.WriteLine(altitude1 - altitude2);
+        }
+    }
+}
