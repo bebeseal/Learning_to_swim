4c921e4db6cf1c1cf8fff861bd70c1c0376ccd6b
Joshua Cole <16261287+bebeseal@users.noreply.github.com>
2022-07-26 22:23:51 -0500

    Cleaned up folder
    Clean up everyone do your share

---
+++ This_is_a_test_script.cs
@@ -0,0 +1,19 @@
+// This is a test script
+
+using System;
+
+    class FirstClass{
+     static void Main() {
+    
+         int [] inputnum = new int[] {-1,1,2,3,4,10};
+            int basenum = 0;}
+
+        foreach(int x in inputnum){
+            if(basenum <= x)
+            {basenum = x}
+            else
+            {
+                basenum = basenum;
+            }
+        }
+    }
